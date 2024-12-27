fdc592da10555000c9490998b7bf528e633e3a0d
wangpx <wangpx@mail.tsinghua.edu.cn>
2024-12-27 15:04:56 +0800

    refactor(config): 更新网站配置和版权信息
    - 修改 SiteFooter组件中的版权年份为动态显示当前年份
    - 更新 SiteFooter 和 SiteNav 组件中的链接地址和网站名称
    - 更改作者信息为"王菜菜"，并更新相应的邮箱地址

--- app/components/SiteFooter.vue
+++ app/components/SiteFooter.vue
@@ -32,7 +32,7 @@
       </n-p>
       <n-p depth="3">
         Copyright &copy; 2020 - {{ new Date().getFullYear() }}
-        <n-text depth="3" @click="jumpLink(linkData.home)"> IMSYY </n-text>
+        <n-text depth="3" @click="jumpLink(linkData.home)"> 王菜菜 </n-text>
         <n-text
           v-if="siteIcp"
           depth="3"
@@ -50,9 +50,9 @@ const { public: configPublic } = useRuntimeConfig();
 const { siteIcp, version } = configPublic;
 
 const linkData = {
-  github: "https://github.com/imsyy/site-status",
-  home: "https://www.imsyy.top",
-  email: "mailto:one@imsyy.top",
+  github: "https://github.com/Cai2w/site-status",
+  home: "https://www.cai2.wang",
+  email: "mailto:1732804469@qq.com",
 };
 </script>
 
